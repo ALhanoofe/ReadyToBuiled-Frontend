4e69708d69c7ee03113da110485f94ddb9fbdc2f
aysha009 <ayshax.009@gmail.com>
2026-02-05 21:24:51 +0300

    done with home css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -21,7 +21,6 @@ const Nav = () => {
             <NavLink to="ProjectForm">Add New Project</NavLink>
             <NavLink to="profile">My Profile</NavLink>
             <NavLink to="/" onClick={handleLogOut}>Logout</NavLink>
-            <NavLink to="ProjectDetails">Project Details</NavLink>
             <NavLink to="Folder">My Folder</NavLink>
 
 
